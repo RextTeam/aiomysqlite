4fff7efc06063c5dc20a9233601199c4654c71fb
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-04-10 19:52:07 +0900

    Create __init__.py

---
+++ aiomysqlite/__init__.py
@@ -0,0 +1,22 @@
+from asqlite import connect
+
+async def create_pool(filename):
+    return Pool(filename)
+
+class Pool:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def acquire(self):
+        return PoolAcquireContextManager(self.filename)
+
+class PoolAcquireContextManager:
+    def __init__(self, filename):
+        self.filename = filename
+
+    async def __aenter__(self):
+        self.connection = await connect(filename)
+        return self.connection
+
+    async def __aexit__(self, *args, **kwargs):
+        await self.connection.close()
